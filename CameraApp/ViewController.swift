584ba9804733894862cd0540f9be04af7e562e60
Khimer Rokovoy <castellio@bk.ru>
2020-11-11 15:06:45 +0800

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Кирилл Романенко on 11.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
